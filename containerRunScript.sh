e53c12b24c4384099c0c4c2576c96f5d0bebd7d4
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2026-04-05 13:13:02 +0530

    exported all variables related to mongoDB

--- containerRunScript.sh
+++ containerRunScript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export MONGO_USERNAME=${{ vars.MONGO_USERNAME }}
+# export MONGO_USERNAME=${{ vars.MONGO_USERNAME }}
 echo "MONGO_USERNAME - $MONGO_USERNAME"
+echo "MONGO_URI = $MONGO_URI"
+echo "MONGO_PASSWORD = $MONGO_PASSWORD"
+
 docker images
