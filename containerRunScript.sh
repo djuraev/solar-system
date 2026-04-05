f10a2ba24fcc5ed0b2b58fb0da45fdbee0c67b10
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2026-04-05 13:20:40 +0530

    changed image tag process

--- containerRunScript.sh
+++ containerRunScript.sh
@@ -13,7 +13,7 @@ docker run -d -p 3000:3000 \
               -e MONGO_URI=$MONGO_URI \
               -e MONGO_USERNAME=$MONGO_USERNAME \
               -e MONGO_PASSWORD=$MONGO_PASSWORD \
-               chowdary2001/solar-system:e53c12b24c4384099c0c4c2576c96f5d0bebd7d4 
+                 $IMAGE
 
 echo testing contianer status
 wget -q -O - localhost:3000/live | grep live
